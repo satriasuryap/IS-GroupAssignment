c4a2f8a98b01e45e3a81d0763d2941df5078371c
Eric Azka Nugroho <eric.a.nugroho@gmail.com>
2023-10-15 13:19:31 +0700

    complete the upload form

--- resources/views/encrypt.blade.php
+++ resources/views/encrypt.blade.php
@@ -3,27 +3,35 @@
 <head>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="bg-gray-200 p-4">
-    <div class="max-w-md mx-auto bg-white p-8 rounded shadow-lg">
-        <h2 class="text-2xl font-semibold mb-4">Upload Form</h2>
+<body class="bg-gradient-to-r from-blue-400 to-red-400 h-screen flex items-center ">
+    <div class="max-w-2xl mx-auto bg-white p-8 rounded shadow-lg">
+        <h2 class="text-2xl font-bold mb-4 text-center">Upload Form</h2>
         <form action="upload.php" method="post" enctype="multipart/form-data">
             <div class="mb-4">
-                <label for="name" class="block text-gray-600">Name</label>
+                <label for="name" class="block text-gray-600 font-semibold">Name</label>
                 <input type="text" name="name" id="name" class="w-full p-2 border rounded">
             </div>
             <div class="mb-4">
-                <label for="email" class="block text-gray-600">Email</label>
+                <label for="email" class="block text-gray-600 font-semibold">Email</label>
                 <input type="email" name="email" id="email" class="w-full p-2 border rounded">
             </div>
             <div class="mb-4">
-                <label for="phone" class="block text-gray-600">Phone Number</label>
+                <label for="phone" class="block text-gray-600 font-semibold">Phone Number</label>
                 <input type="tel" name="phone" id="phone" class="w-full p-2 border rounded">
             </div>
             <div class="mb-4">
-                <label for="files" class="block text-gray-600">Upload Files</label>
-                <input type="file" name="files[]" id="files" class="w-full p-2 border rounded" multiple accept=".pdf, .docx, .xls, .mp4, .mov">
+                <label for="files" class="block text-gray-600 font-semibold">Upload Files for ID Card</label>
+                <input type="file" name="files[]" id="files" class="w-full p-2 border rounded" multiple accept=".png, .jpg, jpeg">
             </div>
-            <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Submit</button>
+            <div class="mb-4">
+                <label for="files" class="block text-gray-600 font-semibold">Upload Files for PDF/DOC/XLS</label>
+                <input type="file" name="files[]" id="files" class="w-full p-2 border rounded" multiple accept=".pdf, .docx, .xls">
+            </div>
+            <div class="mb-4">
+                <label for="files" class="block text-gray-600 font-semibold">Upload Files for Video</label>
+                <input type="file" name="files[]" id="files" class="w-full p-2 border rounded" multiple accept=".mp4, .mov">
+            </div>
+            <button type="submit" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white item-center font-bold py-2 px-4 rounded">Submit</button>
         </form>
     </div>
 </body>
