648cec5ab7d37a24f2e4c97bfc11a5902dc73fe4
satriasuryap <115058337+satriasuryap@users.noreply.github.com>
2023-10-16 22:39:32 +0700

    update databases

--- app/Http/Controllers/EncryptController.php
+++ app/Http/Controllers/EncryptController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Encrypt;
 use Illuminate\Http\Request;
 
 class EncryptController extends Controller
 {
-    //
+
+    public function index(){
+        return view('Encrypt', [
+
+        ]);
+    }
+    public function show($id){
+        return view('Encrypt', [
+            "encrypt" => Encrypt::find($id)
+        ]);
+    }
 }
