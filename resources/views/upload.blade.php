9aaa3bc6e241241c6c756906cdb45f485bd60efd
satriasuryap <115058337+satriasuryap@users.noreply.github.com>
2023-10-16 22:53:14 +0700

    Update upload.blade.php

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -17,19 +17,19 @@
             </div>
             <div class="mb-4">
                 <label for="phone" class="block text-gray-600 font-semibold">Phone Number</label>
-                <input type="tel" name="phone" id="phone" class="w-full p-2 border rounded">
+                <input type="tel" name="phonenum" id="phonenum" class="w-full p-2 border rounded">
             </div>
             <div class="mb-4">
                 <label for="files" class="block text-gray-600 font-semibold">Upload Files for ID Card</label>
-                <input type="file" name="files[]" id="files" class="w-full p-2 border rounded" multiple accept=".png, .jpg, jpeg">
+                <input type="file" name="image[]" id="image" class="w-full p-2 border rounded" multiple accept=".png, .jpg, jpeg">
             </div>
             <div class="mb-4">
                 <label for="files" class="block text-gray-600 font-semibold">Upload Files for PDF/DOC/XLS</label>
-                <input type="file" name="files[]" id="files" class="w-full p-2 border rounded" multiple accept=".pdf, .docx, .xls">
+                <input type="file" name="file[]" id="file" class="w-full p-2 border rounded" multiple accept=".pdf, .docx, .xls">
             </div>
             <div class="mb-4">
                 <label for="files" class="block text-gray-600 font-semibold">Upload Files for Video</label>
-                <input type="file" name="files[]" id="files" class="w-full p-2 border rounded" multiple accept=".mp4, .mov">
+                <input type="file" name="video[]" id="video" class="w-full p-2 border rounded" multiple accept=".mp4, .mov">
             </div>
             <button type="submit" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white item-center font-bold py-2 px-4 rounded">Submit</button>
         </form>
