5316f6b1fb2e56c00ae6f146616d1fcaa472ab3c
Eric Azka Nugroho <eric.a.nugroho@gmail.com>
2023-10-17 18:29:34 +0700

    fixing error on controller

--- app/Http/Controllers/EncryptController.php
+++ app/Http/Controllers/EncryptController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Encrypt;
+use App\Models\upload;
 use Illuminate\Http\Request;
 
 class EncryptController extends Controller
