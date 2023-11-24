feec42dff7dbc80c0ae32947194460009c807d19
Eric Azka Nugroho <eric.a.nugroho@gmail.com>
2023-11-24 19:57:06 +0700

    update UI for user request

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,97 +1,109 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    
-        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
-   <title>Message Encryption</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Message Encryption</title>
 </head>
+
+<body class="bg-gray-100 dark:bg-gray-900">
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1>{{ __("Items List") }}</h1>
+                    <h1 class="text-2xl font-bold mb-4">Database Item List</h1>
 
                     <!-- Display AES Items -->
-                    <h2>AES Items</h2>
-                    <table class="table-auto">
-                        <thead>
-                            <tr>
-                                <th class="px-4 py-2">Name</th>
-                                <th class="px-4 py-2">Email</th>
-                                <th class="px-4 py-2">Phone</th>
-                                <th class="px-4 py-2">Image</th>
-                                <th class="px-4 py-2">File</th>
-                                <th class="px-4 py-2">Video</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($aesItems as $item)
+                    <div class="mb-6 truncate overflow-hidden">
+                        <h2 class="text-xl font-semibold mb-2">AES Items</h2>
+                        <table class="min-w-full bg-white border border-gray-300 text-black truncate overflow-hidden">
+                            <thead>
                                 <tr>
-                                    <td class="px-4 py-2">{{ $item->name }}</td>
-                                    <td class="px-4 py-2">{{ $item->email }}</td>
-                                    <td class="px-4 py-2">{{ $item->phone }}</td>
-                                    <td class="px-4 py-2">{{ $item->img }}</td>
-                                    <td class="px-4 py-2">{{ $item->file }}</td>
-                                    <td class="px-4 py-2">{{ $item->video }}</td>
+                                    <th class="py-2 px-4 border-b">Name</th>
+                                    <th class="py-2 px-4 border-b">Email</th>
+                                    <th class="py-2 px-4 border-b">Phone</th>
+                                    <th class="py-2 px-4 border-b">Image</th>
+                                    <th class="py-2 px-4 border-b">File</th>
+                                    <th class="py-2 px-4 border-b">Video</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($aesItems as $item)
+                                    <tr>
+                                        <td class="py-2 px-4 border-b truncate overflow-hidden">{{ $item->name }}</td>
+                                        <td class="py-2 px-4 border-b truncate overflow-hidden">{{ $item->email }}</td>
+                                        <td class="py-2 px-4 border-b truncate overflow-hidden">{{ $item->phone }}</td>
+                                        <td class="py-2 px-4 border-b truncate overflow-hidden">{{ $item->img }}</td>
+                                        <td class="py-2 px-4 border-b truncate overflow-hidden">{{ $item->file }}</td>
+                                        <td class="py-2 px-4 border-b truncate overflow-hidden">{{ $item->video }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
 
                     <!-- Display DES Items -->
-                    <h2>DES Items</h2>
-                    <table class="table-auto">
-                        <thead>
-                            <tr>
-                                <th class="px-4 py-2">Name</th>
-                                <th class="px-4 py-2">Email</th>
-                                <th class="px-4 py-2">Phone</th>
-                                <th class="px-4 py-2">Image</th>
-                                <th class="px-4 py-2">File</th>
-                                <th class="px-4 py-2">Video</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($desItems as $item)
+                    <div class="mb-6">
+                        <h2 class="text-xl font-semibold mb-2">DES Items</h2>
+                        <table class="min-w-full bg-white border border-gray-300 text-black">
+                            <thead>
                                 <tr>
-                                    <td class="px-4 py-2">{{ $item->name }}</td>
-                                    <td class="px-4 py-2">{{ $item->email }}</td>
-                                    <td class="px-4 py-2">{{ $item->phone }}</td>
-                                    <td class="px-4 py-2">{{ $item->img }}</td>
-                                    <td class="px-4 py-2">{{ $item->file }}</td>
-                                    <td class="px-4 py-2">{{ $item->video }}</td>
+                                    <th class="py-2 px-4 border-b">Name</th>
+                                    <th class="py-2 px-4 border-b">Email</th>
+                                    <th class="py-2 px-4 border-b">Phone</th>
+                                    <th class="py-2 px-4 border-b">Image</th>
+                                    <th class="py-2 px-4 border-b">File</th>
+                                    <th class="py-2 px-4 border-b">Video</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($desItems as $item)
+                                    <tr>
+                                        <td class="py-2 px-4 border-b overflow-hidden">{{ $item->name }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->email }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->phone }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->img }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->file }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->video }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
                     <!-- Display RC4 Items -->
-                    <h2>RC4 Items</h2>
-                    <table class="table-auto">
-                        <thead>
-                            <tr>
-                                <th class="px-4 py-2">Name</th>
-                                <th class="px-4 py-2">Email</th>
-                                <th class="px-4 py-2">Phone</th>
-                                <th class="px-4 py-2">Image</th>
-                                <th class="px-4 py-2">File</th>
-                                <th class="px-4 py-2">Video</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($rc4Items as $item)
+                    <div>
+                        <h2 class="text-xl font-semibold mb-2">RC4 Items</h2>
+                        <table class="min-w-full bg-white border border-gray-300 text-black">
+                            <thead>
                                 <tr>
-                                    <td class="px-4 py-2">{{ $item->name }}</td>
-                                    <td class="px-4 py-2">{{ $item->email }}</td>
-                                    <td class="px-4 py-2">{{ $item->phone }}</td>
-                                    <td class="px-4 py-2">{{ $item->img }}</td>
-                                    <td class="px-4 py-2">{{ $item->file }}</td>
-                                    <td class="px-4 py-2">{{ $item->video }}</td>
+                                    <th class="py-2 px-4 border-b">Name</th>
+                                    <th class="py-2 px-4 border-b">Email</th>
+                                    <th class="py-2 px-4 border-b">Phone</th>
+                                    <th class="py-2 px-4 border-b">Image</th>
+                                    <th class="py-2 px-4 border-b">File</th>
+                                    <th class="py-2 px-4 border-b">Video</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($rc4Items as $item)
+                                    <tr>
+                                        <td class="py-2 px-4 border-b">{{ $item->name }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->email }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->phone }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->img }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->file }}</td>
+                                        <td class="py-2 px-4 border-b">{{ $item->video }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody> 
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</body>
+
 </html>
